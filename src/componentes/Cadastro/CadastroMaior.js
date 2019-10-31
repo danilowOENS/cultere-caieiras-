3c91945a3ff77b436cc142b53e2a3a7d9df848e9
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-31 17:40:48 -0300

    Adicionar componente de Genero

--- src/componentes/Cadastro/CadastroMaior.js
+++ src/componentes/Cadastro/CadastroMaior.js
@@ -1,41 +1,93 @@
 import React, { useState, useEffect } from "react";
 import Input from "../Input/Input";
+import Genero from "../Genero/Genero.js"
 import "./CadastroMaior.scss";
 import Botao from "../Botao/Botao";
 
 const CadastroMaior = () => {
   const [nome, setNome] = React.useState("");
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const [idade, setIdade] = React.useState("");
+  const [cpf, setCpf] = React.useState("");
+  const [cep, setCep] = React.useState("");
+  const [contato, setContato] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [senha, setSenha] = React.useState("");
 
   const salvarMaiorDeDezoito = (event) =>{
     event.preventDefault();
 
-
-  useEffect(() => {
-    fetch(``, {
-      method: "GET"
-    }).then(result => {
-      return result.json()
-    }).then(data => {
-      console.log(data)
-    }).catch(() => {
-      console.error("Erroooooooou, internet ruim ou você que é")
+    fetch('',{
+      method: 'POST',
+      body:JSON.stringify({
+        nome: nome,
+        idade: idade,
+        cpf: cpf,
+        cep: cep,
+        email: email,
+        senha: senha,
+      }),
+      headers:{
+        'Content-Type': 'application/json'
+        }
+    }).then( value => {
+      return value.json()
+    }).then(value => {
+      if(value.id) {
+        alert('Doador criado com sucesso!');
+        
+      } else{
+        alert('Erro ao cadastrar!')
+      }
+      setNome("");
+      setIdade("");
+      setCpf("");
+      setCep("");
+      setEmail("");
+      setSenha("");
     })
-  }, [id])
+  };
+
 
   return (
     <div className="CadastroMaior">
       <h1>Faça seu Cadastro</h1>
-      
-      <p>{mensagem}</p>
-      <form onSubmit={handleSubmit}>
+    
+      <form onSubmit={salvarMaiorDeDezoito}>
         <Input
           value={nome}
           type="text"
           label="Nome"
           placeholder="Digite seu nome completo"
           atualizarState={setNome}
+        />
+         <Input
+          value={idade}
+          type="text"
+          label="Idade"
+          placeholder="Digite sua idade"
+          atualizarState={setIdade}
+        />
+         <Input
+          value={cpf}
+          type="text"
+          label="CPF"
+          placeholder="Digite seu cpf"
+          atualizarState={setCpf}
+        />
+         <Input
+          value={cep}
+          type="text"
+          label="Nome"
+          placeholder="Digite seu nome completo"
+          atualizarState={setCep}
+        />
+        <Genero/>
+         <Input
+          value={contato}
+          type="text"
+          label="Celular"
+          placeholder="Digite o número do seu celular"
+          atualizarState={setContato}
         />
         <Input
           value={email}
@@ -56,5 +108,5 @@ const CadastroMaior = () => {
     </div>
     );
   }
-}
+
 export default CadastroMaior;
---
+++ src/componentes/Genero/Genero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Genero.scss'
+
+const Genero = () => {
+    return (
+        <div >
+            <select className="Genero">
+            <option disabled selected value="">Escolha o sexo</option>
+            <option value="feminino">Feminino</option>
+            <option value="masculino">Masculino</option>
+            <option value="NDF">Não Definido</option>
+            </select>
+        </div>
+    )
+}
+
+export default Genero;
+
+
