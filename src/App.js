58cfe6fbc5068b9e4898e07bc4ad10f7dd2d8489
matheus s. silva <viciogamer157@gmail.com>
2019-10-31 17:47:02 -0300

    fazer pagina de cursos incompleta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import './App.scss';
+import ContainerCursos from './componentes/ContainerCursos/ContainerCursos';
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      
+      <ContainerCursos />
+      <ContainerCursos />
+      <ContainerCursos />
+      <ContainerCursos />
     </BrowserRouter>
   )
 }
